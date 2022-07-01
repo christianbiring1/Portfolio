53a44a24ec69c2b40dc2728c1c2d684465785780
christian <christianbiringanine22@gmail.com>
2022-07-01 11:50:30 +0200

    Validate the form based on user inputs

---
+++ form.js
@@ -0,0 +1,36 @@
+const { form } = document.forms;
+const userName = document.querySelector('#name');
+const userEmail = document.querySelector('#email');
+const message = document.querySelector('#message');
+const warning = document.querySelector('.error');
+
+form.addEventListener('submit', (e) => {
+  const user = userName.value;
+  const email = userEmail.value;
+  const text = message.value;
+  if (user === '') {
+    warning.textContent = 'Fill in the name field';
+    e.preventDefault();
+  }
+  if (email === '') {
+    warning.textContent = 'Fill in the email field';
+    e.preventDefault();
+  }
+
+  if (email !== email.toLowerCase()) {
+    warning.textContent = 'The email must be in lower case';
+    e.preventDefault();
+  }
+  if (text === '') {
+    warning.textContent = 'Id like to hear some from you!';
+    e.preventDefault();
+  }
+  if (text.length < 50) {
+    warning.textContent = 'Thats all you wanted to say? Please at least 50 words';
+    e.preventDefault();
+  }
+  if (user === '' && email === '' && text === '') {
+    warning.textContent = 'Please fill in all the fields';
+    e.preventDefault();
+  }
+});

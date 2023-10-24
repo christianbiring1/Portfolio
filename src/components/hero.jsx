99d2fcd2cab895990dae02b4c6d225764c081cbe
christian <christianbiringanine22@gmail.com>
2023-10-24 16:43:20 +0200

    Add the hero section

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import profil from "../assets/chris.jpg";
 import { FaGithub, FaInstagram, FaTwitter, FaLinkedin } from 'react-icons/fa'
 
@@ -8,15 +9,18 @@ function Hero() {
       <div className="hero_image">
         <img src={profil} alt="chris_picture" />
       </div>
-      <h1>Christian Biringanine</h1>
-      <h4>Full Stack Software Developer</h4>
-      <p>I build accessible, inclusive products and digital experiences for the web.</p>
-
-      <div className="socials">
-        <FaGithub  size={30} color='#333'/>
-        <FaInstagram size={30} />
-        <FaTwitter size={30} />
-        <FaLinkedin size={30} />
+      <div className="hero_intro">
+        <div className="header">
+          <h1 >Christian Biringanine</h1>
+          <h3>Full Stack Software Developer</h3>
+        </div>
+        <p>I build accessible, inclusive products and digital experiences for the web.</p>
+        <div className="socials">
+          <FaGithub  size={30} color='#171515'/>
+          <FaInstagram size={30} color='#d62976' />
+          <FaTwitter size={30} color='#00acee' />
+          <FaLinkedin size={30} color='#0077b5' />
+        </div>
       </div>
     </div>
   );

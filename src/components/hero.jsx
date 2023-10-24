d482cfe7c81638e1df13326ad8da631c5b2e8d50
christian <christianbiringanine22@gmail.com>
2023-10-24 14:54:34 +0200

    Define the color palette

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import profil from "../assets/chris.jpg";
 import { FaGithub, FaInstagram, FaTwitter, FaLinkedin } from 'react-icons/fa'
 
 function Hero() {
   return (
     <div className='hero-container' >
+      <div className="hero_image">
+        <img src={profil} alt="chris_picture" />
+      </div>
       <h1>Christian Biringanine</h1>
       <h4>Full Stack Software Developer</h4>
       <p>I build accessible, inclusive products and digital experiences for the web.</p>

9c92cbc8ceb0d63208da9ef6829f4d52e615a45c
christian <christianbiringanine22@gmail.com>
2024-03-01 17:25:09 +0200

    Using the analytics tracking ID

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/Portfolio/"
+  plugins: [react(), reactRefresh()],
+  define: {
+    'process.env': process.env,
+  },
+  base: ""
 })

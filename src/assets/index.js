47ab7b6d456f0f0d0cb10c2c6d69c180c5451199
christian <christianbiringanine22@gmail.com>
2025-05-20 11:51:39 +0200

    Add mising assets but also export all of the project from one entry point

---
+++ src/assets/index.js
@@ -0,0 +1,27 @@
+import fout from "./fout.png";
+import bookStore from "./bookstore.png";
+import dashboard from "./dashboard.jpg";
+import diceGame from "./dice.png";
+import genuineCharity from "./genuine-charity.png";
+import Luna from "./luna.png";
+import CovidMetrics from "./metrics.png";
+import SmartElimu from "./smartelimu.png";
+import SpaceTraveler from "./space-traveler.png";
+import Todo from "./todo.png";
+import TryKatch from "./tryKatch.png";
+import BudgetApp from "./budget-photo.png";
+
+export {
+  fout,
+  bookStore,
+  BudgetApp,
+  dashboard,
+  diceGame,
+  genuineCharity,
+  Luna,
+  CovidMetrics,
+  SmartElimu,
+  SpaceTraveler,
+  Todo,
+  TryKatch,
+};

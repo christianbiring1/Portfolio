e74e347a9bf386808a6c5669fd52be7d7e709768
christian <christianbiringanine22@gmail.com>
2025-05-20 11:52:52 +0200

    Update the different projects directories to match the changes

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,19 +1,30 @@
-import React, { useRef, useState } from 'react'; //eslint-disable-line
-import { FaEye, FaLaptopCode, FaToolbox, FaTools } from 'react-icons/fa';
-import { BsGear } from 'react-icons/bs'
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React, { useRef, useState } from "react"; //eslint-disable-line
+import {
+  FaEye,
+  FaLaptopCode,
+  FaToolbox,
+  FaTools,
+  FaChevronLeft,
+  FaChevronRight,
+  FaCross,
+  FaLock,
+} from "react-icons/fa";
+import { BsGear } from "react-icons/bs";
+import { Swiper, SwiperSlide } from "swiper/react";
 // import required modules
-import { Keyboard, Pagination, Navigation } from 'swiper/modules';
-import { Modal } from 'react-responsive-modal';
-import PropsTypes from 'prop-types';
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+// import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Modal } from "react-responsive-modal";
+import PropsTypes from "prop-types";
 
-import projects from '../../projectsList';
+import projects from "../../projectsList";
 // import { UseOnClickOutSide } from '../utils/useOnClick';
 
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-
+// import "./styles/projectCaroussel.scss";
+import styles from "./styles/projectCarousel.module.scss";
 
 const Projects = (props) => {
   const { active } = props;
@@ -30,51 +41,103 @@ const Projects = (props) => {
   const handleModal = (e, url, index) => {
     if (!url) {
       e.preventDefault();
-      setActiveIndex(index)
+      setActiveIndex(index);
     }
-  }
-
-  const handleCloseModal = () => {
-    setActiveIndex(null)
   };
 
-  const styles = active ? {color: "#d9d9d9"} : {};
+  const handleCloseModal = () => {
+    setActiveIndex(null);
+  };
+
+  const titleStyle = active ? { color: "#d9d9d9" } : {};
   return (
-    <div className='project_container' id='projects'>
-      <h3 className="title" style={styles}>
-        <span><FaToolbox size={30} /></span>
+    <div className="project_container" id="projects">
+      <h3 className="title" style={titleStyle}>
+        <span>
+          <FaToolbox size={30} />
+        </span>
         <span>Project</span>
       </h3>
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={30}
-        keyboard={{
-          enabled: true,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Keyboard, Pagination, Navigation]}
-        className="mySwiper"
-        // style={active ? {backgroundColor: "#d9d9d9"} : {}}
-      >
-        {projects.map((el, index) => (
+      <div className={styles.carouselWrapper}>
+        <Swiper
+          modules={[Navigation]}
+          spaceBetween={30}
+          slidesPerView={1}
+          // keyboard={{
+          //   enabled: true,
+          // }}
+          speed={500}
+          // pagination={{
+          //   clickable: true,
+          // }}
+          navigation={{
+            nextEl: `.${styles.nextButton}`,
+            prevEl: `.${styles.prevButton}`,
+          }}
+          className={styles.swiper}
+          // style={active ? {backgroundColor: "#d9d9d9"} : {}}
+        >
+          {projects.map((el, index) => (
             <SwiperSlide key={el.id}>
-              <div className='project_content' style={active ? {backgroundColor: "#d9d9d9"} : {}}>
-                <img src={el.image} alt={`${el.projectName}_photo`} />
+              <div
+                className="project_content"
+                style={active ? { backgroundColor: "#d9d9d9" } : {}}
+              >
+                <img
+                  src={el.image}
+                  alt={`${el.projectName}_photo`}
+                  // style={{ width: "90%", height: "auto" }}
+                />
                 <div className="details">
-                  <h2 className='title' >{el.projectName}</h2>
+                  <h2 className="title">{el.projectName}</h2>
                   <div className="description">{el.projectDescription}</div>
-                  <ul className='stack_list'>
-                    {el.stack.map(stack => (
-                      <li key={stack} className='stack_name'>{stack}</li>
+                  <ul className="stack_list">
+                    {el.stack.map((stack) => (
+                      <li key={stack} className="stack_name">
+                        {stack}
+                      </li>
                     ))}
                   </ul>
                   <div className="bottom_cta">
-                    <a href={el.liveDemo} target="_blank" rel="noopener noreferrer" onClick={(e) => handleModal(e, el.liveDemo, index)} ><FaEye  size={25}/></a>
-                    <a href={el.sourceCode} target="_blank" rel="noopener noreferrer"><FaLaptopCode size={25}/></a>
-                    {el.sourceCode2 && <a href={el.sourceCode2} target="_blank" rel="noopener noreferrer"><FaLaptopCode size={25}/></a> }
+                    <a
+                      href={el.liveDemo}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      onClick={(e) => handleModal(e, el.liveDemo, index)}
+                    >
+                      <FaEye size={25} />
+                    </a>
+                    {el.sourceCode ? (
+                      <a
+                        href={el.sourceCode}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <FaLaptopCode size={25} />
+                      </a>
+                    ) : (
+                      <div
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                          gap: "0.5rem",
+                        }}
+                      >
+                        <FaLock size={20} opacity={0.7} />
+                        <span style={{ opacity: "0.7", fontSize: "1.3rem" }}>
+                          Private
+                        </span>
+                      </div>
+                    )}
+                    {el.sourceCode2 && (
+                      <a
+                        href={el.sourceCode2}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <FaLaptopCode size={25} />
+                      </a>
+                    )}
                   </div>
                 </div>
                 <Modal
@@ -82,37 +145,60 @@ const Projects = (props) => {
                   onClose={handleCloseModal}
                   center
                   classNames={{
-                    modalAnimationIn: 'inModal',
-                    modal: 'modal',
+                    modalAnimationIn: "inModal",
+                    modal: "modal",
                   }}
                   animationDuration={500}
                 >
                   <div className="modal-container">
-                    <h2 className='top-head'>Oups!</h2>
+                    <h2 className="top-head">Oups!</h2>
                     <div className="icons">
-                      <BsGear size={100} color='#999' className='spin-icon'/>
-                      <FaTools size={100} color='#c5070799' />
+                      <BsGear size={100} color="#999" className="spin-icon" />
+                      <FaTools size={100} color="#c5070799" />
                     </div>
                     <div className="details">
-                    <p>This project still under development...</p>
-                    <p>
-                      You can check the source code
-                      <a href={el.sourceCode} target="_blank" rel="noopener noreferrer"><FaLaptopCode size={25}/></a>
-                      {el.sourceCode2 && <a href={el.sourceCode2} target="_blank" rel="noopener noreferrer"><FaLaptopCode size={25}/></a> }
-                    </p>
+                      <p>This project still under development...</p>
+                      <p>
+                        You can check the source code
+                        <a
+                          href={el.sourceCode}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <FaLaptopCode size={25} />
+                        </a>
+                        {el.sourceCode2 && (
+                          <a
+                            href={el.sourceCode2}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            <FaLaptopCode size={25} />
+                          </a>
+                        )}
+                      </p>
                     </div>
                   </div>
                 </Modal>
               </div>
             </SwiperSlide>
-        ))}
-      </Swiper>
+          ))}
+        </Swiper>
+        <div className={styles.navigation}>
+          <button className={styles.prevButton}>
+            <FaChevronLeft />
+          </button>
+          <button className={styles.nextButton}>
+            <FaChevronRight />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
 
 Projects.propTypes = {
-  active: PropsTypes.bool.isRequired
-}
- 
+  active: PropsTypes.bool.isRequired,
+};
+
 export default Projects;
--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,11 +1,21 @@
-import  React, { useState } from "react"; //eslint-disable-line
+import React, { useState } from "react"; //eslint-disable-line
 import PropsTypes from "prop-types";
 import { FaReact, FaNodeJs, FaTools } from "react-icons/fa";
 import { RiJavascriptFill } from "react-icons/ri";
 import { TbBrandNextjs } from "react-icons/tb";
-import { SiExpress, SiRuby, SiRubyonrails, SiMongodb, SiPostgresql, SiFigma, SiMui, SiAntdesign, SiCadillac } from "react-icons/si";
+import {
+  SiExpress,
+  SiRuby,
+  SiRubyonrails,
+  SiMongodb,
+  SiPostgresql,
+  SiFigma,
+  SiMui,
+  SiAntdesign,
+  SiCadillac,
+} from "react-icons/si";
 import { BsDatabaseFillGear } from "react-icons/bs";
-import {IoDesktop } from "react-icons/io5";
+import { IoDesktop } from "react-icons/io5";
 
 const Skills = (props) => {
   const { active } = props;
@@ -13,107 +23,121 @@ const Skills = (props) => {
   const frontend = [
     {
       id: 1,
-      label: 'JavaScript',
-      icon: <RiJavascriptFill color="yellow"  size={30}/>,
+      label: "JavaScript",
+      icon: <RiJavascriptFill color="yellow" size={20} />,
     },
     {
       id: 2,
-      label: 'React',
-      icon: <FaReact color='#00acee' size={30}/>,
+      label: "React",
+      icon: <FaReact color="#00acee" size={20} />,
     },
     {
       id: 3,
-      label: 'NextJS',
-      icon: <TbBrandNextjs size={30}/>,
+      label: "NextJS",
+      icon: <TbBrandNextjs size={20} />,
     },
     {
       id: 4,
-      label: 'Material UI',
-      icon: <SiMui size={25}/>
+      label: "Material UI",
+      icon: <SiMui size={16} />,
     },
     {
       id: 5,
-      label: 'Ant design',
-      icon: <SiAntdesign size={25}/>
+      label: "Ant design",
+      icon: <SiAntdesign size={16} />,
     },
     {
       id: 6,
-      label: 'Chadcn.ui',
-      icon: < SiCadillac size={25}/>
-    }
+      label: "Chadcn.ui",
+      icon: <SiCadillac size={16} />,
+    },
   ];
   const backend = [
     {
       id: 1,
-      label: 'NodeJS',
-      icon: <FaNodeJs size={30} color="green" />
+      label: "NodeJS",
+      icon: <FaNodeJs size={20} color="green" />,
     },
     {
       id: 2,
-      label: 'Express',
-      icon: <SiExpress size={30} />
+      label: "Express",
+      icon: <SiExpress size={20} />,
     },
     {
       id: 3,
-      label: 'Ruby',
-      icon: <SiRuby size={30} color="red" />
+      label: "Ruby",
+      icon: <SiRuby size={20} color="red" />,
     },
     {
       id: 4,
-      label: 'Rails',
-      icon: <SiRubyonrails size={30} color="red" />
-    }
+      label: "Rails",
+      icon: <SiRubyonrails size={20} color="red" />,
+    },
   ];
 
   const tools = [
     {
       id: 1,
-      label: 'MongoDB',
-      icon: <SiMongodb  size={30} color="green" />
+      label: "MongoDB",
+      icon: <SiMongodb size={20} color="green" />,
     },
     {
       id: 2,
-      label: 'PostgreSQL',
-      icon: <SiPostgresql  size={30} color="#1b0760" />
+      label: "PostgreSQL",
+      icon: <SiPostgresql size={20} color="#1b0760" />,
     },
     {
       id: 3,
-      label: 'Figma',
-      icon: <SiFigma  size={30} color="#ae4dff" />
+      label: "Figma",
+      icon: <SiFigma size={20} color="#ae4dff" />,
     },
   ];
-  const styles = active ? {color: "#d9d9d9"} : {};
+  const styles = active ? { color: "#d9d9d9" } : {};
   return (
     <div className="skills_container">
       <div className="front-end">
-        <h3 style={styles}><IoDesktop color = {active ? "#d9d9d9" : "#333"} size={20}/> Front-End</h3>
+        <h3 style={styles}>
+          <IoDesktop color={active ? "#d9d9d9" : "#333"} size={20} /> Front-End
+        </h3>
         <div className="front-end-skills">
-          {frontend.map(item => (
-            <div key={item.id} className="skill_item" >
+          {frontend.map((item) => (
+            <div key={item.id} className="skill_item">
               <span> {item.icon}</span>
-              <span style={{ fontFamily: 'Raleway', fontSize: '1.3rem' }}>{item.label}</span>
+              <span style={{ fontFamily: "Raleway", fontSize: "1.3rem" }}>
+                {item.label}
+              </span>
             </div>
           ))}
         </div>
       </div>
       <div className="back-end">
-        <h3 style={styles}> <BsDatabaseFillGear color={active ? "#d9d9d9" : "blue"}/> Backend</h3>
+        <h3 style={styles}>
+          {" "}
+          <BsDatabaseFillGear color={active ? "#d9d9d9" : "blue"} /> Backend
+        </h3>
         <div className="front-end-skills">
-          {backend.map(item => (
-            <div key={item.id} className="skill_item" >
+          {backend.map((item) => (
+            <div key={item.id} className="skill_item">
               <span> {item.icon}</span>
-              <span style={{ fontFamily: 'Raleway', fontSize: '1.3rem' }}>{item.label}</span>
+              <span style={{ fontFamily: "Raleway", fontSize: "1.3rem" }}>
+                {item.label}
+              </span>
             </div>
           ))}
         </div>
       </div>
       <div className="back-end">
-        <h3 style={styles}> <FaTools color={active ? "#d9d9d9" : "#333"} /> Tools</h3>
+        <h3 style={styles}>
+          {" "}
+          <FaTools color={active ? "#d9d9d9" : "#333"} /> Tools
+        </h3>
         <div className="front-end-skills">
-          {tools.map(item => (
-            <div key={item.id} className="skill_item" >
+          {tools.map((item) => (
+            <div key={item.id} className="skill_item">
               <span> {item.icon}</span>
-              <span style={{ fontFamily: 'Raleway', fontSize: '1.3rem' }}>{item.label}</span>
+              <span style={{ fontFamily: "Raleway", fontSize: "1.3rem" }}>
+                {item.label}
+              </span>
             </div>
           ))}
         </div>
@@ -123,7 +147,7 @@ const Skills = (props) => {
 };
 
 Skills.propTypes = {
-  active: PropsTypes.bool.isRequired
-}
- 
+  active: PropsTypes.bool.isRequired,
+};
+
 export default Skills;

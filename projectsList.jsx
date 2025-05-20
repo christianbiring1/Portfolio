6f24f2b2cd13a1ea1583b6c754de56e3a2be820d
christian <christianbiringanine22@gmail.com>
2025-05-20 11:50:54 +0200

    Update the project list

--- projectsList.jsx
+++ projectsList.jsx
@@ -1,166 +1,171 @@
-import fout from "./src/assets/fout.png";
-import spacetraveler from "./src/assets/space-traveler.png";
-import dice from "./src/assets/dice.png";
-import voty from "./src/assets/voty.png";
-import bookstore from "./src/assets/bookstore.png";
-import budget from "./src/assets/budget-photo.png";
-import genuine from "./src/assets/genuine-charity.png";
-import hospisync from "./src/assets/hospisync.png";
-import metrics from "./src/assets/metrics.png";
-import todo from "./src/assets/todo.png";
-import dashboard from "./src/assets/dashboard.jpg";
+// import fout from "./src/assets/fout.png";
+// import spacetraveler from "./src/assets/space-traveler.png";
+// import dice from "./src/assets/dice.png";
+// import voty from "./src/assets/voty.png";
+// import bookstore from "./src/assets/bookstore.png";
+// import budget from "./src/assets/budget-photo.png";
+// import genuine from "./src/assets/genuine-charity.png";
+// import hospisync from "./src/assets/hospisync.png";
+// import metrics from "./src/assets/metrics.png";
+// import todo from "./src/assets/todo.png";
+// import dashboard from "./src/assets/dashboard.jpg";
+
+import {
+  bookStore,
+  BudgetApp,
+  CovidMetrics,
+  dashboard,
+  diceGame,
+  fout,
+  genuineCharity,
+  Luna,
+  SpaceTraveler,
+  SmartElimu,
+  Todo,
+  TryKatch,
+} from "./src/assets/index";
 
 export default [
+  {
+    image: SmartElimu,
+    id: "02",
+    projectName: "Smart Elimu",
+    stack: ["React", "Redux@toolkit", "scss"],
+    projectDescription:
+      "Smart Elimu is a web application where schools can manage their educational system including classes, students, the staff and their finances",
+    liveDemo: "https://silver-nougat-d7e1fc.netlify.app/",
+    sourceCode: "",
+  },
+  {
+    image: Luna,
+    id: "01",
+    projectName: "LUNA web platform",
+    stack: ["React", "ReactQuery", "Redux@toolkit", "MUI", "scss", "Firebase"],
+    projectDescription:
+      "Luna is  a web platform for women health including a shop where they can boy health related product,can book consultations with a professional but also where they can read blogs related topics",
+    liveDemo: "https://lunahealth.co/",
+    sourceCode: "",
+  },
+  {
+    image: TryKatch,
+    id: "03",
+    projectName: "TryKatch web platform",
+    stack: ["Nextjs", "Strapi", "Tailwind"],
+    projectDescription:
+      "TryKatch is a web application for the TryKtach Development team where client can get all their related services but also it contains a blog posting where different topics can be discussed",
+    liveDemo: "https://www.trykatch.net",
+    sourceCode: "",
+  },
   {
     image: fout,
     id: 1,
     projectName: "Font Out",
-    stack: [
-      "React",
-      "TailWind",
-      "Google Fonts API"
-    ],
-    projectDescription: "This project is about building a web application where developers can customize the Font they wanna use and get the output properties they can later copy to their codebase",
+    stack: ["React", "TailWind", "Google Fonts API"],
+    projectDescription:
+      "This project is about building a web application where developers can customize the Font they wanna use and get the output properties they can later copy to their codebase",
     liveDemo: "https://silver-nougat-d7e1fc.netlify.app/",
-    sourceCode: "https://github.com/christianbiring1/Font_out"
+    sourceCode: "https://github.com/christianbiring1/Font_out",
   },
   {
     image: dashboard,
     id: 11,
     projectName: "Dashboard",
-    stack: [
-      "React",
-      "MUI",
-      "TailWind",
-      "Recharts"
-    ],
-    projectDescription: "This project is about building a Admin dashboard of random web app",
+    stack: ["React", "MUI", "TailWind", "Recharts"],
+    projectDescription:
+      "This project is about building a Admin dashboard of random web app",
     liveDemo: "https://dashboardui-demo.netlify.app/",
-    sourceCode: "https://github.com/christianbiring1/Dashboardui"
+    sourceCode: "https://github.com/christianbiring1/Dashboardui",
   },
   {
-    image: spacetraveler,
+    image: SpaceTraveler,
     id: 2,
     projectName: "Space Traveler",
-    stack: [
-      "ReactJS",
-      "Redux",
-      "SpaceX API",
-      "SCSS"
-    ],
-    projectDescription: "In this project, I have worked with the real live data from the SpaceX API to build a web application for a company that provides commercial and scientific space travel services. The application will allow users to book rockets and join selected space missions.",
+    stack: ["ReactJS", "Redux", "SpaceX API", "SCSS"],
+    projectDescription:
+      "In this project, I have worked with the real live data from the SpaceX API to build a web application for a company that provides commercial and scientific space travel services. The application will allow users to book rockets and join selected space missions.",
     liveDemo: "https://fancy-elf-c0882d.netlify.app/",
-    sourceCode: "https://github.com/christianbiring1/space_traveler"
+    sourceCode: "https://github.com/christianbiring1/space_traveler",
   },
   {
-    image: dice,
+    image: diceGame,
     id: 3,
     projectName: "Dice Game",
-    stack: [
-      "ReactJS",
-      "react-confetti"
-    ],
-    projectDescription: "Web application Game where users can play the dice game just for fun. They will be able to keep of the time it took 'em to complete the game and No. of times they roll to get the correct number. Thay will be able to beat their record as well.",
+    stack: ["ReactJS", "react-confetti"],
+    projectDescription:
+      "Web application Game where users can play the dice game just for fun. They will be able to keep of the time it took 'em to complete the game and No. of times they roll to get the correct number. Thay will be able to beat their record as well.",
     liveDemo: "https://glowing-donut-fcb7c8.netlify.app",
-    sourceCode: "https://github.com/christianbiring1/hospiSync"
+    sourceCode: "https://github.com/christianbiring1/hospiSync",
   },
+  // {
+  //   image: hospisync,
+  //   id: 4,
+  //   projectName: "HospiSync",
+  //   stack: ["NodeJS", "MongoDB", "React", "MUI", "Tailwind"],
+  //   projectDescription:
+  //     "This project is about building web application where hospitals can manage their stuffs: patients, staff, finances and more",
+  //   liveDemo: "",
+  //   sourceCode: "https://github.com/christianbiring1/hospiSync",
+  // },
+  // {
+  //   image: voty,
+  //   id: 5,
+  //   projectName: "E voty",
+  //   stack: ["MongoDB", "Express", "ReactJS", "NodeJS"],
+  //   projectDescription:
+  //     "This project is about building web application where users can choose a candidate who's running for a specific election after logging with their ID",
+  //   liveDemo: "",
+  //   sourceCode: "https://github.com/christianbiring1/fictional-octo-waddle",
+  //   sourceCode2: "https://github.com/christianbiring1/vote_api",
+  // },
   {
-    image: hospisync,
-    id: 4,
-    projectName: "HospiSync",
-    stack: [
-      "NodeJS",
-      "MongoDB",
-      "React",
-      "MUI",
-      "Tailwind"
-    ],
-    projectDescription: "This project is about building web application where hospitals can manage their stuffs: patients, staff, finances and more",
-    liveDemo: "",
-    sourceCode: "https://github.com/christianbiring1/hospiSync"
-  },
-  {
-    image: voty,
-    id: 5,
-    projectName: "E voty",
-    stack: [
-      "MongoDB",
-      "Express",
-      "ReactJS",
-      "NodeJS"
-    ],
-    projectDescription: "This project is about building web application where users can choose a candidate who's running for a specific election after logging with their ID",
-    liveDemo: "",
-    sourceCode: "https://github.com/christianbiring1/fictional-octo-waddle",
-    sourceCode2: "https://github.com/christianbiring1/vote_api"
-  },
-  {
-    image: budget,
+    image: BudgetApp,
     id: 6,
     projectName: "Budget App",
-    stack: [
-      "Ruby on Rails",
-      "PostgreSQL",
-      "Bootstrap",
-      "CSS"
-    ],
-    projectDescription: "Is web application where users can manage their budget: they have a list of transactions associated with a category, so that they can see how much money they spent and on what.",
+    stack: ["Ruby on Rails", "PostgreSQL", "Bootstrap", "CSS"],
+    projectDescription:
+      "Is web application where users can manage their budget: they have a list of transactions associated with a category, so that they can see how much money they spent and on what.",
     liveDemo: "https://budget-app-4zi2.onrender.com",
-    sourceCode: "https://github.com/christianbiring1/Budget_App"
+    sourceCode: "https://github.com/christianbiring1/Budget_App",
   },
   {
-    image: metrics,
+    image: CovidMetrics,
     id: 7,
     projectName: "Covid-19 Metrics",
-    stack: [
-      "ReactJS",
-      "Redux",
-      "Covid API",
-      "CSS"
-    ],
-    projectDescription: "This project is about building a static site to check the list of metrics data about a the covid-19 views case in the world per country",
+    stack: ["ReactJS", "Redux", "Covid API", "CSS"],
+    projectDescription:
+      "This project is about building a static site to check the list of metrics data about a the covid-19 views case in the world per country",
     liveDemo: "https://glittering-centaur-5e199f.netlify.app",
     sourceCode: "https://github.com/christianbiring1/metrics-webapp",
-    "class": "covidApp"
+    class: "covidApp",
   },
   {
-    image: bookstore,
+    image: bookStore,
     id: 8,
     projectName: "BookStore",
-    stack: [
-      "React",
-      "Redux"
-    ],
-    projectDescription: "The Bookstore is a website that allows users to: Display a list of books. Add a book.Remove a selected book",
+    stack: ["React", "Redux"],
+    projectDescription:
+      "The Bookstore is a website that allows users to: Display a list of books. Add a book.Remove a selected book",
     liveDemo: "https://kaleidoscopic-flan-8dba08.netlify.app",
-    sourceCode: "https://github.com/christianbiring1/bookstore"
+    sourceCode: "https://github.com/christianbiring1/bookstore",
   },
   {
-    image: todo,
+    image: Todo,
     id: 9,
     projectName: "To Do list",
-    stack: [
-      "JavaScript",
-      "LocalStorage",
-      "Webpack",
-      "HTML5",
-      "CSS3"
-    ],
-    projectDescription: "A simple To Do app where users will be able to add, edit and delete their created todos list.",
+    stack: ["JavaScript", "LocalStorage", "Webpack", "HTML5", "CSS3"],
+    projectDescription:
+      "A simple To Do app where users will be able to add, edit and delete their created todos list.",
     liveDemo: "https://development--stalwart-dango-a6b44a.netlify.app/",
-    sourceCode: "https://github.com/christianbiring1/To-Do_list"
+    sourceCode: "https://github.com/christianbiring1/To-Do_list",
   },
   {
-    image: genuine,
+    image: genuineCharity,
     id: 10,
     projectName: "Genuine Charity",
-    stack: [
-      "JavaScript",
-      "HTML5",
-      "CSS3"
-    ],
-    projectDescription: "Genuine Charity website to enhance their presence online, where user can find latest news, infos and how to contribute in the organization",
+    stack: ["JavaScript", "HTML5", "CSS3"],
+    projectDescription:
+      "Genuine Charity website to enhance their presence online, where user can find latest news, infos and how to contribute in the organization",
     liveDemo: "https://christianbiring1.github.io/Capstone_project-module1/",
-    sourceCode: "https://github.com/christianbiring1/Capstone_project-module1"
-  }
-]
+    sourceCode: "https://github.com/christianbiring1/Capstone_project-module1",
+  },
+];
